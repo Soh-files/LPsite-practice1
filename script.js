c5d420783776f77247d9b56b66ae65dbef2e6fda
そーたろう <81458517+Soh-files@users.noreply.github.com>
2021-06-12 20:44:36 +0900

    2021-06-12 Adding contents and javascript.
    続きの項目を追加
    javascriptで動作の追加
    サイト完成

--- script.js
+++ script.js
@@ -1,19 +1,96 @@
-const $Icon = document.querySelectorAll('.flowicon');
-const $Text = document.querySelectorAll('.flowtext');
+document.addEventListener('DOMContentLoaded', function () {
 
-for(i = 0; i < $Icon.length; i++) {
-    $Icon[i].addEventListener('click', function(e) {
-        e.preventDefault();
-        this.classList.toggle('open');
-        this.nextElementSibling.classList.toggle('open');
-    });
-};
+    // 資料請求のコンテンツまでスクロールする
+    document.addEventListener('scroll', function() {
+        const $contactForm = document.getElementById('contactform');
+        const contactHeight = $contactForm.getBoundingClientRect();
+        const contactTop = contactHeight.top + window.pageYOffset;
+        const $contactBtn = document.querySelectorAll('.contact');
+        const $contactBtn2 = document.getElementById('contactbtn');
+    
+        for(let i = 0; i < $contactBtn.length; i++) {
+            $contactBtn[i].addEventListener('click', function (e) {
+                e.preventDefault();
+                document.documentElement.scrollTop = contactTop - 100;
+            });
+        }
+        $contactBtn2.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.documentElement.scrollTop = contactTop - 100;
+        });
+    })
 
-const $question = document.querySelectorAll('.question');
-for (j = 0; j < $question.length; j++) {
-    $question[j].addEventListener('click', function(e) {
-        e.preventDefault();
-        this.classList.toggle('open');
-        this.nextElementSibling.classList.toggle('open');
+    // モバイル画面ではiSaraの流れをアコーディオンメニューで表示する
+    const $icon = document.querySelectorAll('.flowicon');
+    const flowContentHeight = [];
+    
+    for(let j = 0; j < $icon.length; j++) {
+        flowContentHeight[j] = $icon[j].nextElementSibling.clientHeight + "px";
+        $icon[j].nextElementSibling.style.height = flowContentHeight[j] + "px";
+
+        if ($icon[j].nextElementSibling.classList.contains('closed')) {
+            $icon[j].nextElementSibling.style.height = "0px";
+        };
+        $icon[j].addEventListener('click', function(e) {
+            let num = Array.prototype.indexOf.call($icon, this);
+            e.preventDefault();
+            this.classList.toggle('open');
+            this.nextElementSibling.classList.toggle('open');
+            this.nextElementSibling.classList.toggle('closed');
+            if(this.nextElementSibling.classList.contains('open')) {
+                this.nextElementSibling.style.height = flowContentHeight[num];
+            } else {
+                this.nextElementSibling.style.height = "0px";
+            }
+        });
+    };
+    
+    // Q&Aの項目はアコーディオンで表示する
+    const $question = document.querySelectorAll('.question');
+    const AnswerContentHeight = [];
+
+    for(let k = 0; k < $question.length; k++) {
+        AnswerContentHeight[k] = $question[k].nextElementSibling.clientHeight + "px";
+        $question[k].nextElementSibling.style.height = AnswerContentHeight[k] + "px";
+
+        if (!$question[k].nextElementSibling.classList.contains('open')) {
+            $question[k].nextElementSibling.style.height = "0px";
+            $question[k].nextElementSibling.style.padding = "0";
+        };
+
+        $question[k].addEventListener('click', function(e) {
+            let index = Array.prototype.indexOf.call($question, this);
+            e.preventDefault();
+            this.classList.toggle('open');
+            this.nextElementSibling.classList.toggle('open');
+            if(this.nextElementSibling.classList.contains('open')) {
+                this.nextElementSibling.style.height = AnswerContentHeight[index];
+                this.nextElementSibling.style.padding = "20px 15px";
+            } else {
+                this.nextElementSibling.style.height = "0px";
+                this.nextElementSibling.style.padding = "0";
+            }
+        });
+        console.log(AnswerContentHeight[k]);
+    };
+
+    // 画面下部のボタンでページトップに戻る
+    let pageHeight;
+    document.addEventListener('scroll', function(){
+        pageHeight = window.pageYOffset;
     });
-}
+
+    const $topBtn = document.getElementById('top-btn');
+    let timer = null;
+
+    function setTimer () {
+        if (pageHeight === 0) {
+            clearInterval(timer);
+        }
+        window.scrollTo(0, pageHeight - 200);
+    }
+    $topBtn.addEventListener('click', function () {
+        timer = setInterval(setTimer, 10);
+    });
+});
+
